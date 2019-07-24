ea578232951019ccc0da1f67b354c999e0f40261
MaxL34 <mlauvergeat@gmail.com>
2019-07-24 10:23:47 +0200

    Suppression de commentaires

--- controllers/frontController.php
+++ controllers/frontController.php
@@ -74,18 +74,6 @@ function displayOnBoard() {
             "NOVEMBRE" => $progManager->getProg_nov(),
             "DECEMBRE" => $progManager->getProg_dec() 
         ];
-        /*$prog_jan = $progManager->getProg_jan();
-        $prog_feb = $progManager->getProg_feb();
-        $prog_mar = $progManager->getProg_mar();
-        $prog_apr = $progManager->getProg_apr();
-        $prog_may = $progManager->getProg_may();
-        $prog_jun = $progManager->getProg_jun();
-        $prog_jul = $progManager->getProg_jul();
-        $prog_aug = $progManager->getProg_aug();
-        $prog_sep = $progManager->getProg_sep();
-        $prog_oct = $progManager->getProg_oct();
-        $prog_nov = $progManager->getProg_nov();
-        $prog_dec = $progManager->getProg_dec();*/
 
         $mapManager = new App\Map\MapManager();
         $mapEvents = $mapManager->displayEvents();
